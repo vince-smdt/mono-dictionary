34c123b162041946186b0caafe43b45b1c5a4bc7
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-03-07 19:20:20 -0500

    Add word lookup

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import dictionary
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for
 
 # Flask application
 app = Flask(__name__)
 
 # Routes
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/word-submit", methods=['GET', 'POST'])
+def word_submit():
+    if request.method == 'POST':
+        result = request.form['word']
+        return redirect(url_for('word_definition', word=result))
+    
+    return redirect(url_for('home'))
+
 @app.route("/<word>")
 def word_definition(word : str):
     word_data = dictionary.fetch_word(word)
-
+    
     if word_data == dictionary.INVALID_MESSAGE:
-        return "<h2>Invalid word!</h2>"
-
-    return "<p>" + str(word_data) + "</p>"
+        return "<p>Invalid word!</p>"
+    
+    return render_template("definition.html", data=word_data)
