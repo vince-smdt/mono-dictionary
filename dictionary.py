df727d1bba00193b22432b628944ae4084195098
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-03-10 14:40:06 -0500

    Switch to wiktionary api

--- dictionary.py
+++ dictionary.py
@@ -1,16 +1,17 @@
 import requests.api as api
 
-INVALID_MESSAGE = "No Definitions Found"
+INVALID_MESSAGE = "Page or revision not found."
 
 def fetch_word(word : str):
     if not word:
         return INVALID_MESSAGE
     
     # Finding word info with api call
-    payload = api.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
+    payload = api.get("https://en.wiktionary.org/api/rest_v1/page/definition/" + word)
     data = payload.json()
+    print(data)
 
-    if type(data) == dict and data["title"] == INVALID_MESSAGE:
+    if "detail" in data and data["detail"] == INVALID_MESSAGE:
         return INVALID_MESSAGE
 
-    return data
+    return data["en"] # TODO - Let user choose language
