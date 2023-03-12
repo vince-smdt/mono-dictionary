61c7f582db6843248c1eaf4f72f509d07bd0dd58
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-03-12 17:38:47 -0400

    Show synonyms and antonyms

--- frontend/src/components/WordDefinitionComponent.vue
+++ frontend/src/components/WordDefinitionComponent.vue
@@ -1,17 +1,72 @@
 <template>
-  <ol id="outer-list">
+  <!-- Sense -->
+  <ol id="word-sense-list">
     <li v-for="sense in data" :key="sense">
-      {{ sense.word }}, phonetic: <span>{{ sense.phonetic }}</span>
+
+      <!-- Phonetic -->
+      {{ sense.word }}, phonetic:
+      <span id="phonetic">{{ sense.phonetic }}</span>
+
+      <!-- Meaning -->
       <ol>
         <li v-for="meaning in sense.meanings" :key="meaning">
           part of speech: {{ meaning.partOfSpeech }}
+
+          <!-- Definition -->
           <ol>
             <li v-for="definition in meaning.definitions" :key="definition">
               {{ definition.definition }}
+
+              <!-- Definition synonyms -->
+              <div v-if="definition.synonyms.length">
+                Synonyms
+                <ul>
+                  <li v-for="synonym in definition.synonyms" :key="synonym">
+                    {{ synonym }}
+                  </li>
+                </ul>
+              </div>
+
+              <!-- Definition antonyms -->
+              <div v-if="definition.antonyms.length">
+                Antonyms
+                <ul>
+                  <li v-for="antonym in definition.antonyms" :key="antonym">
+                    {{ antonym }}
+                  </li>
+                </ul>
+              </div>
+
+              <!-- Definition example -->
+              <span v-if="'example' in definition">
+                Example: {{ definition.example }}
+              </span>
             </li>
           </ol>
+
+          <!-- Meaning synonyms -->
+          <div v-if="meaning.synonyms.length">
+            Synonyms
+            <ul>
+              <li v-for="synonym in meaning.synonyms" :key="synonym">
+                {{ synonym }}
+              </li>
+            </ul>
+          </div>
+
+          <!-- Meaning antonyms -->
+          <div v-if="meaning.antonyms.length">
+            Antonyms
+            <ul>
+              <li v-for="antonym in meaning.antonyms" :key="antonym">
+                {{ antonym }}
+              </li>
+            </ul>
+          </div>
+
         </li>
       </ol>
+
     </li>
   </ol>
 </template>
@@ -26,8 +81,13 @@ export default {
 </script>
 
 <style scoped>
-#outer-list {
+#word-sense-list {
   max-width: 80em;
+}
+
+#phonetic {
+  font-family: "Times New Roman", Times, serif;
+  font-size: 22px;
 }
 
 ol {
@@ -43,10 +103,14 @@ ol > li {
   margin-bottom: 0.6em;
 }
 
+ul > li {
+  margin-left: 2em;
+}
+
 ol > li:before {
   content: counters(item, ".") ". ";
   display: table-cell;
-  padding-right: 0.6em;    
+  padding-right: 0.6em;
 }
 
 li ol > li {
@@ -55,9 +119,5 @@ li ol > li {
 
 li ol > li:before {
   content: counters(item, ".") " ";
-}
-
-span {
-  font-family: "Times New Roman", Times, serif;
 }
 </style>
