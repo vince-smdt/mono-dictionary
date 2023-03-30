9232ffd17f2e1a239c9fe68a3b52c2cface6a96f
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-03-30 18:54:11 -0400

    Add light mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,16 +12,31 @@ export default {
 </script>
 
 <style>
+
 :root {
-  --text: white;
-  --text-hover: #c9c9d6;
-  --background: #061329;
-  --background-hover: #0c2146;
-  --background-light: #0c1b36;
-  --background-lighter: #132549;
-  --blockquote: #224c94;
-  --border: white;
+    --text: black;
+    --text-hover: #4d4d5b;
+    --background: white;
+    --background-light: #eeeeee;
+    --background-lighter: #cccccc;
+    --background-hover: #e6e6e6;
+    --blockquote: #848484;
+    --border: black;
+  }
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --text: white;
+    --text-hover: #c9c9d6;
+    --background: #061329;
+    --background-light: #0c1b36;
+    --background-lighter: #132549;
+    --background-hover: #0c2146;
+    --blockquote: #224c94;
+    --border: white;
+  }
 }
+
 
 @font-face {
   font-family: "Roboto Mono";
